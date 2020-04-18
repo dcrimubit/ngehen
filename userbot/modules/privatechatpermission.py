01ca25380a788006ebee241690e841157ca8768b
Jefanya Efandchris <63415527+jefanyae@users.noreply.github.com>
2020-04-18 21:31:23 +0700

    Update privatechatpermission.py

--- userbot/modules/privatechatpermission.py
+++ userbot/modules/privatechatpermission.py
@@ -156,7 +156,8 @@ async def approvepm(apprvpm):
     except IntegrityError:
         return await apprvpm.edit("`User ini mungkin sudah di tag dalam transaksi.`")
 
-    await apprvpm.edit(f"[{name0}](tg://user?id={uid}) `approved to PM!`")
+    await apprvpm.edit(f"`Starting Bot ...!`\n"
+                       f"`Sedang Melakukan Transaksi Dengan`[{name0}](tg://user?id={uid})")
 
     async for message in apprvpm.client.iter_messages(apprvpm.chat_id,
                                                       from_user='me',
